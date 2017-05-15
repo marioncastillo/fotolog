935a95d95d27d6862d84c3a4f0e1565288e85380
MarionFoto <marionacp@gmail.com>
2017-05-15 12:49:35 -0400

    ahora si funciona

--- js/main.js
+++ js/main.js
@@ -2,40 +2,42 @@ function agregaPosteo() {
 //escribe aca el codigo
 var containerPosteos = document.getElementById("contenedor-posteos");
 var nombreFF = document.getElementById("nombre").value;
-var contenedorFF = documentElementById("cajaposteos").value;
+var contenedorFF = document.getElementById("cajaposteos").value;
+
 var nuevoPost = document.createElement("div");
+
 var contenedorNombre = document.createElement("strong");
 var contenedorPost = document.createElement("P");
-var separacion = documen.createElement("hr");
+var separacion = document.createElement("hr");
 
-var parraCorazon = document.createElement('P');
+var parrafCorazon = document.createElement('P');
 var i = document.createElement('i');
-var parraCorazon.appendChild('i');
+parrafCorazon.appendChild(i);
 
-parraCorazon.setAttribute("class", "corazon");
+parrafCorazon.setAttribute("class", "corazon");
 i.setAttribute("class","fa fa-heart");
 i.setAttribute("arial-hidden","true");
 
-var nodoNombre = document.createTexNode(nombreFF + "escribió:");
-var nodoPosteo = document.createTexNode(contenedorFF);
+var nodoNombre = document.createTextNode(nombreFF + " escribió:");
+var nodoPosteo = document.createTextNode(contenedorFF);
 
-contenedorNombre.oppenChild(nodoNombre);
-contenedorPost.oppenChild(nodoPosteo);
+contenedorNombre.appendChild(nodoNombre);
+contenedorPost.appendChild(nodoPosteo);
 
-nuevoPost.oppenChild(contenedorNombre);
-nuevoPost.oppenChild(contenedorPost);
-nuevoPost.oppenChild(parraCorazon);
-nuevoPost.oppenChild(separacion);
+nuevoPost.appendChild(contenedorNombre);
+nuevoPost.appendChild(contenedorPost);
+nuevoPost.appendChild(parrafCorazon);
+nuevoPost.appendChild(separacion);
 
-i.addEventListener("click", function()) {
+i.addEventListener("click", function() {
 	i.classList.toggle('rojo');
 
 });
 
 nuevoPost.setAttribute("class","posteo");
-containerPosteos.oppenChild(nuevoPost);
+containerPosteos.appendChild(nuevoPost);
 
 document.getElementById("nombre").value = "";
-container.getElementById("cajaposteos").value = "";
+document.getElementById("cajaposteos").value = "";
 
 }
